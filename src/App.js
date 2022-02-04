2aaae9bb93cb6f067d15c5e66b2052ea6b7f85ca
unknown <samuelbankole779@gmail.com>
2022-02-04 07:52:54 +0100

    Project moved to new repo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import { getUser } from "./utils/utils";
+import LandingPage from "./pages/landing/Landingpage.component";
+import Login from "./pages/login/Login.component";
+import Baseview from "./pages/baseview/Baseview.component";
+import Dashboard from "./pages/dashboard/Dashboard.component";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route
+          path="/"
+          element={!getUser("name") ? <LandingPage /> : <Baseview />}
+        />
+        <Route
+          path="/login"
+          element={
+            !getUser("name") ? (
+              <Login logAcc="Login as Help Desktop Agent" />
+            ) : (
+              <Baseview />
+            )
+          }
+        />
+        <Route
+          path="/login-ad"
+          element={
+            !getUser("name") ? <Login logAcc="Login as Admin" /> : <Baseview />
+          }
+        />
+        <Route
+          path="/dashboard"
+          element={getUser("name") ? <Baseview /> : <LandingPage />}
+        />
+        <Route
+          path="/home"
+          element={getUser("name") ? <Dashboard /> : <LandingPage />}
+        />
+      </Routes>
+    </>
   );
-}
+};
 
 export default App;
